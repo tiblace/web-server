6aa556ccc2ef339a4853cc9b20f48d619ea6ac91
Sheryl Selby-Spear <48160556+tiblace@users.noreply.github.com>
2019-04-01 16:45:36 -0500

    change message

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8888;
 
-app.get('/', (req, res) => res.send('Hello Mankato!'));
+app.get('/', (req, res) => res.send('On the master branch!'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
