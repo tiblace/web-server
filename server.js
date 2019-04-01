4223442ff08b8af1f7c6a87bf8544eed03d0e940
Sheryl Selby-Spear <48160556+tiblace@users.noreply.github.com>
2019-04-01 16:44:20 -0500

    modify message

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8888;
 
-app.get('/', (req, res) => res.send('Hello Mankato!'));
+app.get('/', (req, res) => res.send('This is front-end!'));
 
 app.listen(port);
